1dbf4646e09458e8d25a6aa2ece1aa01c661c64c
Vatsal Manot <vatsal.manot@yahoo.com>
2025-04-13 13:42:04 -0700

    Update package

--- Package.swift
+++ Package.swift
@@ -1,15 +1,25 @@
-// swift-tools-version:5.7
+// swift-tools-version:5.10
 import PackageDescription
 
 let package = Package(
     name: "SourceKitten",
     platforms: [.macOS(.v12)],
     products: [
-        .executable(name: "sourcekitten", targets: ["sourcekitten"]),
-        .library(name: "SourceKittenFramework", targets: ["SourceKittenFramework"])
+        .executable(
+            name: "sourcekitten",
+            targets: [
+                "sourcekitten"
+            ]
+        ),
+        .library(
+            name: "SourceKittenFramework",
+            targets: [
+                "SourceKittenFramework"
+            ]
+        )
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.1"),
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.5.0"),
         .package(url: "https://github.com/preternatural-fork/Yams.git", branch: "main"),
         .package(url: "https://github.com/vmanot/CorePersistence.git", branch: "main"),
     ],
